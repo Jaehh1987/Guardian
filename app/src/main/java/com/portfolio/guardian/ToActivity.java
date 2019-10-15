35a07ac441d99b23dc8a527d5fc9ef74a601dbb0
LAPTOP-G5629H2U\Jae Hyunhyo <jaehh1987@gmail.com>
2019-10-15 14:18:04 -0700

    Add intents to connect from, to and map activity Implement simple google map into map activity

--- app/src/main/java/com/portfolio/guardian/ToActivity.java
+++ app/src/main/java/com/portfolio/guardian/ToActivity.java
@@ -1,6 +1,8 @@
 package com.portfolio.guardian;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class ToActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to);
+    }
+
+    public void moveMap(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
     }
 }
